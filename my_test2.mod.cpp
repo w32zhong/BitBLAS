ce52643f5bbf21ab209e5c16a32b5fcd6e926abc
w32zhong <clock126@126.com>
2024-07-02 21:35:39 -0400

    update

---
+++ my_test2.mod.cpp
@@ -0,0 +1,52 @@
+// tvm target: c -keys=cpu 
+#define TVM_EXPORTS
+#include "tvm/runtime/c_runtime_api.h"
+#include "tvm/runtime/c_backend_api.h"
+#include <math.h>
+#include <stdbool.h>
+#ifdef __cplusplus
+extern "C"
+#endif
+TVM_DLL int32_t main(void* args, int32_t* arg_type_ids, int32_t num_args, void* out_ret_value, int32_t* out_ret_tcode, void* resource_handle);
+#ifdef __cplusplus
+extern "C"
+#endif
+typedef uint16_t half;
+TVM_DLL int32_t main(void* args, int32_t* arg_type_ids, int32_t num_args, void* out_ret_value, int32_t* out_ret_tcode, void* resource_handle) {
+  int32_t A_handle_code = arg_type_ids[0];
+  int32_t B_handle_code = arg_type_ids[1];
+  int32_t B_decode_handle_code = arg_type_ids[2];
+  void* A_handle = (((TVMValue*)args)[0].v_handle);
+  void* B_handle = (((TVMValue*)args)[1].v_handle);
+  void* B_decode_handle = (((TVMValue*)args)[2].v_handle);
+  void* main_A_handle_shape = (((DLTensor*)A_handle)[0].shape);
+  void* main_A_handle_strides = (((DLTensor*)A_handle)[0].strides);
+  int32_t dev_id = (((DLTensor*)A_handle)[0].device.device_id);
+  void* A = (((DLTensor*)A_handle)[0].data);
+  void* main_B_handle_shape = (((DLTensor*)B_handle)[0].shape);
+  void* main_B_handle_strides = (((DLTensor*)B_handle)[0].strides);
+  void* B = (((DLTensor*)B_handle)[0].data);
+  void* main_B_decode_handle_shape = (((DLTensor*)B_decode_handle)[0].shape);
+  void* main_B_decode_handle_strides = (((DLTensor*)B_decode_handle)[0].strides);
+  void* B_decode = (((DLTensor*)B_decode_handle)[0].data);
+  if (!(main_A_handle_strides == NULL)) {
+  }
+  if (!(main_B_handle_strides == NULL)) {
+  }
+  if (!(main_B_decode_handle_strides == NULL)) {
+  }
+  for (int32_t n = 0; n < 1024; ++n) {
+    for (int32_t k = 0; k < 1024; ++k) {
+      ((half*)B_decode)[((n * 1024) + k)] = (((half)((((uint32_t)((int8_t*)B)[((n * 256) + (k >> 2))]) >> (((uint32_t)(k & 3)) * (uint32_t)2)) & (uint32_t)3)) - (half)2.000000e+00f);
+    }
+  }
+  return 0;
+}
+
+// CodegenC: NOTE: Auto-generated entry function
+#ifdef __cplusplus
+extern "C"
+#endif
+TVM_DLL int32_t __tvm_main__(void* args, int* arg_type_ids, int num_args, void* out_ret_value, int* out_ret_tcode, void* resource_handle) {
+  return main(args, arg_type_ids, num_args, out_ret_value, out_ret_tcode, resource_handle);
+}
--- my_test2.mod.py
+++ my_test2.mod.py
@@ -17,7 +17,6 @@ class Module:
         for n, k in T.grid(1024, 1024):
             with T.block("B_decode"):
                 v_n, v_k = T.axis.remap("SS", [n, k])
-                print(v_n, v_k)
                 T.reads(B[v_n, v_k // 4])
                 T.writes(B_decode[v_n, v_k])
                 B_decode[v_n, v_k] = T.Cast("float16", T.bitwise_and(T.shift_right(T.Cast("uint32", B[v_n, v_k // 4]), T.Cast("uint32", v_k % 4) * T.uint32(2)), T.uint32(3))) - T.float16(2)
@@ -25,10 +24,10 @@ class Module:
 mod = tvm.build(Module, target="c")
 with open('/dev/stdout', 'w') as fh:
     fh.write(mod.get_source())
-mod = tvm.build(Module, target="llvm")
 
-a = tvm.nd.array(np.zeros((1, 1024)).astype("float16"))
-b = tvm.nd.array(np.zeros((1024, 256)).astype("int8"))
-c = tvm.nd.array(np.zeros((1024, 1024)).astype("float16"))
-mod(a, b, c)
-print(c)
+#mod = tvm.build(Module, target="llvm")
+#a = tvm.nd.array(np.zeros((1, 1024)).astype("float16"))
+#b = tvm.nd.array(np.zeros((1024, 256)).astype("int8"))
+#c = tvm.nd.array(np.zeros((1024, 1024)).astype("float16"))
+#mod(a, b, c)
+#print(c)
